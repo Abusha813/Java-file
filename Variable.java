e260994f126ea9079d8e41a240c1b4716ece58a9
Abusha <abusha8134@gmail.com>
2025-04-17 20:29:53 +0300

    variable

---
+++ Variable.java
@@ -0,0 +1,35 @@
+// /**Author...Ahmed
+//  * Date of creation 21-03-2024
+//  * vertion 1.0 name Avarage.java
+//  * debre birhan university
+//  * this program works describing local variable,instance variable and static or class variable
+//  * 
+//  */
+// public class Variable {
+  
+ 
+ 
+//   void variable(String name3){
+//     String name1=name3;
+//     System.out.println("name: "+name1);
+//   }
+  
+//   void variable2(String name2){
+//     static String name22=name2; 
+//     System.out.println("name: "+name22);
+//   }
+  
+//   void var(String name1){
+//     String ename3 = name1;//instance variable
+//     System.out.println("name: "+ename3);
+//   }
+// public static void main(String[] args) {
+//     Variable variable=new Variable();
+//     Variable Name1=new variable(name3="Ahmed");
+//     Variable Name2=new variable2(name2="Ahmed");
+//     Variable Name3=new var(name1="Ahmed");
+    
+    
+
+// }
+// }
